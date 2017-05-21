b0e1dc05a02200df0b01c6719b6b4947510a1cdd
Ted Shaowang <tedswjz@gmail.com>
2017-05-21 20:36:58 +0800

    added search box

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -5,19 +5,20 @@ var app = getApp();
 
 Page({
   data: {
-        inputShowed: false,
-        inputVal: "",
-        courses: [],
-        skipped: 0,
-        loading: false
+    inputShowed: false,
+    inputVal: "",
+    courses: [],
+    skipped: 0,
+    loading: false,
+    search: false
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function() {
+  onReady: function () {
 
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
     this.setData({
       loading: true
@@ -29,49 +30,73 @@ Page({
       .then(courses => this.setData({ courses }))
       .catch(console.error);
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   },
 
   showInput: function () {
-      this.setData({
-          inputShowed: true
-      });
-  },
-  hideInput: function () {
-      this.setData({
-          inputVal: "",
-          inputShowed: false
-      });
-  },
-  clearInput: function () {
-      this.setData({
-          inputVal: ""
-      });
-  },
-  inputTyping: function (e) {
-      this.setData({
-          inputVal: e.detail.value
-      });
-  },
-  toDetail: function(e) {
-    wx.navigateTo({
-      url: '../detail/index?'+'objectId='+e.currentTarget.dataset.id
+    this.setData({
+      inputShowed: true
     });
   },
-  addCourse: function(e){
-    app.globalData.course=null;
+  hideInput: function () {
+    this.setData({
+      inputShowed: false,
+      skipped: 0,
+      loading: true,
+      inputVal: "",
+      search: false
+    })
+    new AV.Query('Course')
+      .limit(10)
+      .descending('updatedAt')
+      .find()
+      .then(courses => this.setData({ courses }))
+      .catch(console.error);
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    var that = this;
+    this.setData({
+      inputVal: e.detail.value,
+      search: true
+    });
+    var query = new AV.SearchQuery('Course');
+    query.sortBy(new AV.SearchSortBuilder().descending('updatedAt'));
+    query.queryString(e.detail.value);
+    query.find().then(function(courses){
+      console.log('Found %d objects', query.hits());
+      that.setData({courses});
+    }).catch(function (err) {
+      console.log("search error");
+    });
+    this.setData({
+      loading: false
+    })
+  },
+  toDetail: function (e) {
+    wx.navigateTo({
+      url: '../detail/index?' + 'objectId=' + e.currentTarget.dataset.id
+    });
+  },
+  addCourse: function (e) {
+    app.globalData.course = null;
     wx.navigateTo({
       url: '../rate/index'
     });
   },
-  onPullDownRefresh: function(){
+  onPullDownRefresh: function () {
     this.setData({
       skipped: 0,
-      loading: true
+      loading: true,
+      inputVal: ""
     })
     new AV.Query('Course')
       .limit(10)
@@ -81,29 +106,35 @@ Page({
       .catch(console.error);
     wx.stopPullDownRefresh();
   },
-  onReachBottom: function(){
-    var that = this;
-    this.setData({
-      skipped: this.data.skipped + 10,
-      loading: true
-    })
-    new AV.Query('Course')
-      .limit(10)
-      .skip(that.data.skipped)
-      .descending('updatedAt')
-      .find()
-      .then(courses => this.setData({
-        courses: that.data.courses.concat(courses)
-      }))
-      .catch(console.error);
-    this.setData({
-      loading: false
-    })
+  onReachBottom: function () {
+    if (!this.data.search) {
+      var that = this;
+      this.setData({
+        skipped: this.data.skipped + 10,
+        loading: true
+      })
+      new AV.Query('Course')
+        .limit(10)
+        .skip(that.data.skipped)
+        .descending('updatedAt')
+        .find()
+        .then(courses => this.setData({
+          courses: that.data.courses.concat(courses)
+        }))
+        .catch(console.error);
+      this.setData({
+        loading: false
+      })
+    } else {
+      this.setData({
+        loading: false
+      })
+    }
   },
   onShareAppMessage: function () {
-      return {
-          title: '选什么课',
-          path: '/pages/browse/index'
-      }
+    return {
+      title: '选什么课',
+      path: '/pages/browse/index'
+    }
   }
 });
